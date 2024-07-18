f41b8712da79fd839f0741797e98b50535b881d1
guzelberkay <guzelberkay@outlook.com>
2024-07-18 15:09:17 +0300

    001 proje 1.gün

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import {BrowserRouter,Route,Routes} from 'react-router-dom';
+import Login from './pages/login/login';
+import Home from './pages/home/Home';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const router = 
+<BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/login' element={<Login/>} />
+  </Routes>
+</BrowserRouter>
+
+root.render(router);
 
